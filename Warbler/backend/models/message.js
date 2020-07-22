72d12915162361298e745eb6196e0527bd92518d
siddhant <siddhant10shukla@gmail.com>
2020-07-23 05:06:58 +0530

    Add authentication and messages model and finish the backend

---
+++ Warbler/backend/models/message.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const User = require("./user");
+
+const messageSchema = new mongoose.Schema({
+    text: {
+      type: String,
+      required: true,
+      maxlength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    
+  },
+  {
+    timestamps: true
+  }
+);
+
+messageSchema.pre('remove', async function(next){
+  try{
+    //find a user
+    let user = await User.findById(this.user);
+    user.message.remove(this.id);
+    await user.save();
+    return next();
+  } catch(e){
+    return next(err);
+  }
+});
+
+const Message = mongoose.model('Message', messageSchema);
+module.exports = Message;
